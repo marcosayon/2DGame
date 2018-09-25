3ff9b33c5f651bf95e3c7773b1224fb15c1a0447
Marcos Ayon <marcosayon@email.arizona.edu>
2018-09-25 14:49:54 -0700

    2D Game Environment and GUI

---
+++ GameDev2D Game/Assets/Scripts/MenuBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuBehavior : MonoBehaviour {
+    
+    //will play the first level when play button is clicked
+    public void PlayGame(){
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    //will open the options menu when options button is clicked
+    public void Options(){
+        SceneManager.LoadScene("OptionsMenu");
+    }
+
+    //will quit the game when quit button is clicked
+    public void QuitGame(){
+        Application.Quit();
+    }
+
+}

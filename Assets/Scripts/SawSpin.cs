e07dd8fcc052738cee6e6b2473a49f80a9a8ab9b
Marcos Ayon <marcosayon@email.arizona.edu>
2018-10-04 16:37:07 -0700

    Added animations to project

---
+++ Assets/Scripts/SawSpin.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawSpin : MonoBehaviour {
+    
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(Vector3.right, Time.deltaTime * 100);
+	}
+}
